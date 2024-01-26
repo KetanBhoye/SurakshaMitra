581c6d74b5e0aa91313f3a2b7ef5334d0a44c5fa
Ketan <imketanbhoye100@gmail.com>
2024-01-26 13:10:37 +0530

    image added

--- app/src/main/java/com/example/surakshamitra/LoginSignupScreen.kt
+++ app/src/main/java/com/example/surakshamitra/LoginSignupScreen.kt
@@ -1,11 +1,30 @@
 package com.example.surakshamitra
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginSignupScreen : AppCompatActivity() {
+    private lateinit var createAccount: Button
+    private lateinit var loginUser: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (supportActionBar != null) {
+            supportActionBar?.hide();
+        }
         setContentView(R.layout.activity_login_signup_screen)
+
+        createAccount = findViewById(R.id.createloginBtn)
+        loginUser = findViewById(R.id.loginBtn)
+
+        createAccount.setOnClickListener {
+            intent = Intent(this, RegisterUsers::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
