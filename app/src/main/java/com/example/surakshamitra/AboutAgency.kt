6efa85ca0045313d0c435406aefcbee4ff721771
Vaishnavi Lokhande <lokhande.vaishnavi27@gmail.com>
2024-01-27 14:06:33 +0530

    Agency Info and User Profile completed

---
+++ app/src/main/java/com/example/surakshamitra/AboutAgency.kt
@@ -0,0 +1,11 @@
+package com.example.surakshamitra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutAgency : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_agency)
+    }
+}
